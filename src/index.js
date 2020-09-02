8832944b31a7ed2d50f330d3115027a42a222754
AlexeyGorbachevskiy <alexeygorbachevskiyy@gmail.com>
2020-09-02 13:11:29 +0300

    feat: implement of reverse-int task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
-module.exports = function reverse (n) {
-  
+
+
+// module.exports = function reverse(n) {
+//     return Number(Math.sqrt(n ** 2).toString().split('').reverse().join(''))
+// }
+
+module.exports = function reverse(n) {
+    let num = Math.sqrt(n ** 2);
+    let result = 0;
+    while (num) {
+        result = result * 10 + num % 10;
+        num = Math.floor(num / 10)
+    }
+    return result
+
 }
